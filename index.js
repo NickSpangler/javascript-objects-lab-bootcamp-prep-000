b1d989f2b5381ce945953d9ed591093804f74af0
Nick Spangler <spangleyman@mac.com>
2020-05-08 19:48:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var myObject = {prop: "1"}
 
+function UpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
  object[key] = value
  return object
